310a974fc9bc500351bb8940280899b15eadf10c
Sanna Luukkonen <sanna.luukkonen@softagram.com>
2023-02-06 13:12:30 +0200

    Compile

--- dist/modelapi.d.ts
+++ dist/modelapi.d.ts
@@ -1,5 +1,10 @@
 import { SElement } from './selement';
 import { SGraph } from './sgraph';
+export declare enum FilterAssociations {
+    Ignore = 1,
+    Direct = 2,
+    DirectAndIndirect = 3
+}
 declare class ModelApi {
     model: SGraph;
     egm: SGraph;
@@ -15,5 +20,24 @@ declare class ModelApi {
     getCallingFunctions: (element: SElement) => SElement[];
     getUsedElements: (element: SElement) => SElement[];
     getUserElements: (element: SElement) => SElement[];
+    createDescendants: (relatedElement: SElement, newOrExistingReferredElement: SElement) => void;
+    /**
+        Filter a sub graph from source graph related to source element.
+        When executing filterModel for element e with "Ignore" mode, it ignores elements
+        that are external to e.
+        "Direct" mode changes this behavior: it picks also external elements associated
+        with descendants of e. However, it is limited to direct assocations.
+        "DirectAndIndirect" mode selects all directly and indirectly associated elements
+        and their descendants.
+        Traversal of "DirectAndIndirect" is not directed, i.e. it goes in undirected mode,
+        leading into situations where you have the dependencies of your users also included.
+        @param {SElement} sourceElement element for filtering (descendants of it)
+        @param {SGraph} sourceGraph the overall graph to be filtered
+        @param {FilterAssociations} filterOutgoing filtering mode for outgoing dependencies
+        @param {FilterAssociations} filterIncoming filtering mode for incoming dependencies
+        @returns {SGraph} new graph, that contains independent new SElement objects with same
+        topology as in the source graph
+        */
+    filterModel: (sourceElement: SElement, sourceGraph: SGraph, filterOutgoing?: FilterAssociations, filterIncoming?: FilterAssociations) => SGraph;
 }
 export { ModelApi };
--- dist/sgraph/sgraph.d.ts
+++ dist/sgraph/sgraph.d.ts
@@ -18,6 +18,17 @@ declare class SGraph {
     static parseXml({ data, ignoredAttributes, onlyRoot, }: ParseXmlOptions): SGraph;
     static parseXmlFileOrZippedXml({ filePath, typeRules, ignoredAttributes, onlyRoot, }: ParseXmlFileOrZippedXmlOptions): Promise<SGraph | undefined>;
     createOrGetElementFromPath(n: string): SElement;
+    createOrGetElement(elem: SElement): SElement;
+    /**
+        Create or get element matching this element, yielding also boolean
+        to indicate if new was created.
+        @param element the element to create or get
+        @returns tuple of the matched element and boolean describing if new element was created.
+        */
+    createOrGetElementWithNew(element: SElement): {
+        element: SElement;
+        isNew: boolean;
+    };
     findElementFromPath(i: string): SElement | undefined;
     setModelPath(filePath: string): void;
     toEcharts(): EChartsOptions;
