2c71da9abeced0307481f9a7c58f6d224e20a5d9
Sanna Luukkonen <sanna.luukkonen@softagram.com>
2023-02-10 11:31:19 +0200

    Compile bug fixes

--- dist/selement/selementAssociation.d.ts
+++ dist/selement/selementAssociation.d.ts
@@ -4,6 +4,21 @@ declare class SElementAssociation {
     toElement: SElement;
     deptype?: string;
     attrs?: any;
+    /**
+       Create association between two elements if there already does not exist a similar association.
+       The association is considered to be similar if toElement has an incoming association with the
+       same type and the same fromElement.
+       @param {SElement} fromElement the elemenet that is the starting point of the association
+       @param {SElement} toElement the element that is the ending point of the association
+       @param {string} deptype the type of the association
+       @param {any} depattrs attributes for the associtaion
+       @returns {SElement, boolean} Return an object containing the existing or new element and a
+       boolean indicating if a new element was created (true if new was created, false otherwise)
+     */
+    static createUniqueElementAssociation(fromElement: SElement, toElement: SElement, deptype?: any, depattrs?: any): {
+        existingOrNewAssociation: SElementAssociation;
+        isNew: boolean;
+    };
     constructor(from: SElement, to: SElement, deptype?: any, depattrs?: any);
     calculateCompareStatus(): 1 | 0 | 2 | 3;
     initElems(): void;
